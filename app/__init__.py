5e27c4c745648296b23ddfcfa4e4d2411c9277e9
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-01-24 00:40:25 -0300

    refactor: simplify app initialization and remove database setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,16 @@
 import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+from flask import Flask
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///videos.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SECRET_KEY"] = os.urandom(24)  # Gera uma chave aleatória
 
-    db.init_app(app)
+    app.config["SECRET_KEY"] = os.urandom(24)
 
     with app.app_context():
         from .routes import bp
 
         app.register_blueprint(bp)
-        db.create_all()
 
     return app
